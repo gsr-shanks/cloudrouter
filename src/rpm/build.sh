75296a6827a05fc6265551cb1f312dad789806c7
David Jorm <david.jorm@gmail.com>
2015-02-02 17:23:03 +1000

    Remove minor version from ODL spec file name

--- src/rpm/build.sh
+++ src/rpm/build.sh
@@ -5,4 +5,4 @@ cp SOURCES/* ~/rpmbuild/SOURCES/
 curl -o ~/rpmbuild/SOURCES/RPM-GPG-KEY-CLOUDROUTER https://cloudrouter.org/repo/RPM-GPG-KEY-CLOUDROUTER
 rpmbuild -ba --clean ~/rpmbuild/SPECS/cloudrouter-1.0.spec
 curl -o ~/rpmbuild/SOURCES/opendaylight-helium.zip https://cloudrouter.org/repo/beta/src/opendaylight-helium.zip
-rpmbuild -ba --clean ~/rpmbuild/SPECS/opendaylight-helium-sr1.1.spec
+rpmbuild -ba --clean ~/rpmbuild/SPECS/opendaylight-helium.spec
